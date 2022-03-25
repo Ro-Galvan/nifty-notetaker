fc7af1bb983e2678c485074822e8fbfe2b42f396
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-03-24 22:14:08 -0400

    added environment variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express'); // we are requiring express module
 const path = require('path'); //requiring path module
 
 const app = express();  //we take express and execute it--we get an express application
-const PORT = 3001; //set a const for a port number-- this will never change by denotting them in all CAPS
+const PORT = process.env.PORT || 3001; //set a const for a port number-- this will never change by denotting them in all CAPS
 
 app.use(express.static('public'));  // middleware-what happens in between request and responses on express server. we are telling the app to serve a static directory called public--this will allow and expose the html files as public info on internet
 
